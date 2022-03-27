d822dc92c09658ad983af21df93c4cd1a9937c80
Leonardo Rios <leo78rios@gmail.com>
2022-03-27 12:43:56 -0700

    first commit

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from time import clock_settime
 from django.views.generic.edit import CreateView
-from django.views.generic  import ListView, DetailView
+from django.views.generic  import (ListView, DetailView,
+UpdateView, CreateView)
 from .models import Post
 
 class PostListView(ListView):
@@ -14,4 +15,9 @@ class PostDetailView(DetailView):
 class PostCreateView(CreateView):
     template_name = "posts/new.html"
     model = Post
+    fields = ['title', 'text']
+
+class PostUpdateView(UpdateView):
+    template_name = "post/edit.html"
+    models = Post
     fields = ['title', 'text']
